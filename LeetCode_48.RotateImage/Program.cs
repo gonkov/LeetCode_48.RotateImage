679f5fe1748f1b229f4b0f3c2d06c56a5d89e3e8
gonkov <gonkov@gmail.com>
2022-11-15 00:14:18 +0300

    input and output realization

--- LeetCode_48.RotateImage/Program.cs
+++ LeetCode_48.RotateImage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,43 @@ namespace LeetCode_48.RotateImage
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //input
+            string s = Console.ReadLine().Trim();
+            s = s.Replace("[[", "").Replace("]]", "");
+            string[] stringSeparators = new string[] { "],[" };
+            List<string> list = s.Split(stringSeparators,0).ToList();
+            int n = list.Count;
+            List<List<int>> table = new List<List<int>>();
+            foreach (string item in list)
+            {
+                table.Add(item.Split(',').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList());
+            }
+
+            // main function
+
+            //output
+            Console.Write("[");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("[");
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(table[i][j]);
+                    if (j != n - 1)
+                    {
+                        Console.Write(",");
+                    }
+                    
+                }
+                Console.Write("]");
+                if (i != n - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.Write("]");
+            Console.ReadLine();
         }
     }
 }
